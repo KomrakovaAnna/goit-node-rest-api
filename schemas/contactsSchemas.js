bf61aa5d72c52228606142e40f84c3d17ff113e6
Anna Komrakova <komrakova.anna@gmail.com>
2025-04-20 20:16:29 +0100

    hw3

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -4,14 +4,23 @@ export const createContactSchema = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().required(),
   phone: Joi.string().required(),
+  favorite: Joi.bool(),
 });
 
 export const updateContactSchema = Joi.object({
   name: Joi.string(),
   email: Joi.string(),
   phone: Joi.string(),
+  favorite: Joi.bool(),
 })
   .min(1)
   .messages({
     'object.min': 'Body must have at least one field',
   });
+
+export const updateStatusContactSchema = Joi.object({
+  name: Joi.string(),
+  email: Joi.string(),
+  phone: Joi.string(),
+  favorite: Joi.bool().required(),
+});
--- services/contactsServices.js
+++ services/contactsServices.js
@@ -1,52 +1,30 @@
-import fs from 'node:fs/promises';
-import path from 'node:path';
-import { nanoid } from 'nanoid';
+import Contact from '../db/models/Contact.js';
 
-const contactsPath = path.resolve('db', 'contacts.json');
-const updateContacts = contacts =>
-  fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+export const getContacts = () => Contact.findAll();
 
-export const getContacts = async () => {
-  const data = await fs.readFile(contactsPath, 'utf-8');
-  return JSON.parse(data);
+export const getContactById = id => Contact.findByPk(id);
+
+export const addContact = contact => Contact.create(contact);
+
+export const updateContactById = async (id, data) => {
+  const contact = await getContactById(id);
+  if (!contact) return null;
+  return contact.update(data, {
+    returning: true,
+  });
 };
 
-export const getContactById = async contactId => {
-  const contacts = await getContacts();
-  const result = contacts.find(contact => contact.id === contactId);
-  return result || null;
+export const deleteContact = id =>
+  Contact.destroy({
+    where: {
+      id,
+    },
+  });
+
+export const updateStatusContact = async (contactId, body) => {
+  const contact = await getContactById(contactId);
+  if (!contact) return null;
+  return contact.update(body, {
+    returning: true,
+  });
 };
-
-export async function addContact({ name, email, phone }) {
-  const contacts = await getContacts();
-  const newContact = {
-    id: nanoid(),
-    name,
-    email,
-    phone,
-  };
-  contacts.push(newContact);
-  await updateContacts(contacts);
-
-  return newContact;
-}
-
-export const updateContactById = async (contactId, contact) => {
-  const contacts = await getContacts();
-  const index = contacts.findIndex(item => item.id === contactId);
-  if (index === -1) return null;
-  contacts[index] = { ...contacts[index], ...contact };
-
-  await updateContacts(contacts);
-
-  return contacts[index];
-};
-
-export async function deleteContact(contactId) {
-  const contacts = await getContacts();
-  const contact = contacts.findIndex(contact => contact.id === contactId);
-  if (contact === -1) return null;
-  const [result] = contacts.splice(contact, 1);
-  await updateContacts(contacts);
-  return result;
-}
